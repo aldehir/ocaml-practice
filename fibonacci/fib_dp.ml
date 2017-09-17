e97a4ba2f579ed3532f15d55d481ee2ac0aea863
aldehir <hello@aldehir.com>
2017-09-16 23:36:42 -0500

    Implement dynamic programming fibonacci sequence, no for loop

---
+++ fibonacci/fib_dp.ml
@@ -0,0 +1,32 @@
+let fib n =
+  match n with
+  | 0 | 1 -> n
+  | _ ->
+    let a = ref 0 in
+    let b = ref 1 in
+    let result = ref 0 in
+    let rec iterate x =
+      match x with
+      | 1 -> ()
+      | _ ->
+        result := !a + !b;
+        a := !b;
+        b := !result;
+        iterate (x - 1)
+    in
+    iterate n;
+    !result
+
+let print_usage () =
+  print_endline ("Usage: " ^ Sys.argv.(0) ^ " number")
+
+let main () =
+  match Array.length Sys.argv with
+  | 1 -> print_usage ()
+  | _ ->
+    let n = int_of_string Sys.argv.(1) in
+    let result = fib n in
+    print_int result; print_newline ()
+
+let () =
+  main ()
